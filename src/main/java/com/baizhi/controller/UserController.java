d2c1c1c7972dbfb155355d1cd01a302ebfde659a
yk <1135750073@qq.com>
2019-12-06 19:17:57 +0800

    用户模块登录功能实现

---
+++ src/main/java/com/baizhi/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.baizhi.controller;
+
+import com.baizhi.entity.User;
+import com.baizhi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("login")
+    public Map login(User user) {
+        return userService.findOne(user);
+    }
+}
